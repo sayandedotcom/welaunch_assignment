53489fbdb55578dcf81e375ea315918e2fdeb7d7
Sayan De <sayandeten@gmail.com>
2026-06-08 02:36:55 +0530

    feat: integrate ThemeProvider into AppProviders

--- app/components/providers/AppProviders.tsx
+++ app/components/providers/AppProviders.tsx
@@ -3,13 +3,16 @@
 import { ReactNode } from 'react';
 import { WorkspaceProvider } from '@/context/WorkspaceContext';
 import { ChatProvider } from '@/context/ChatContext';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <WorkspaceProvider>
-      <ChatProvider>
-        {children}
-      </ChatProvider>
-    </WorkspaceProvider>
+    <ThemeProvider>
+      <WorkspaceProvider>
+        <ChatProvider>
+          {children}
+        </ChatProvider>
+      </WorkspaceProvider>
+    </ThemeProvider>
   );
 }
